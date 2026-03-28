c637a7e28e4c3ca3a28950ac7d81a352a8a944a0
Ian Kimeli <iankimeli@gmail.com>
2026-03-28 07:26:53 +0300

    Update UI and functionality for employee management app

---
+++ main.js
@@ -0,0 +1,17 @@
+function details() {
+alert("Viewing employee details");
+}
+
+function edit() {
+alert("Edit employee information");
+}
+
+function deleteContact() {
+
+let confirmDelete = confirm("Are you sure you want to delete?");
+
+if(confirmDelete){
+alert("Employee deleted");
+}
+
+}
